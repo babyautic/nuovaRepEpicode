00b5c0fbd195bd328bab549f0bca91f0d512076f
babyautic <dannytamayo2003@gmail.com>
2025-01-27 00:12:46 +0100

    add file esame

---
+++ W4D1_esame/index.js
@@ -0,0 +1,31 @@
+const multipleItemCarousel = document.querySelector('#carouselExample');
+
+if (window.matchMedia("(min-width:576px)").matches) {
+    var carouselWidth = $('.carousel-inner')[0].scrollWidth;
+    var cardWidth = $('.carousel-item').width();
+
+    var scrollPosition = 0;
+
+    $('.carousel-control-next').on('click', function () {
+        const carousel = new bootstrap.Carousel(multipleItemCarousel, {
+            interval: false
+        })
+        if (scrollPosition < (carouselWidth - (cardWidth * 4))) {
+            console.log('next');
+            scrollPosition = scrollPosition + cardWidth;
+            $('.carousel-inner').animate({ scrollLeft: scrollPosition }, 200);
+        }
+
+    });
+
+    $('.carousel-control-prev').on('click', function () {
+        if (scrollPosition > 0) {
+            console.log('prev');
+            scrollPosition = scrollPosition - cardWidth;
+            $('.carousel-inner').animate({ scrollLeft: scrollPosition }, 200);
+        }
+
+    });
+} else{
+    $(multipleItemCarousel).addClass('slide');
+}
